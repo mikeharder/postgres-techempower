29d142b0a3f1c24b1d1533f3e71e62dbffefb266
Mike Harder <mharder@microsoft.com>
2016-11-04 12:50:05 -0700

    Update fortunesbench.sh

--- postgres-techempower-fortunesbench/fortunesbench.sh
+++ postgres-techempower-fortunesbench/fortunesbench.sh
@@ -2,6 +2,6 @@
 
 set timeout [expr $env(PGBENCH_TIME) * 2]
 
-spawn pgbench -h $env(PGBENCH_HOST) -U benchmarkdbuser -T $env(PGBENCH_TIME) -n -M $env(PGBENCH_MODE) -f fortunes.sql -j $env(PGBENCH_THREADS) -c $env(PGBENCH_CONNECTIONS) hello_world
+spawn pgbench -h $env(PGBENCH_HOST) -U benchmarkdbuser -T $env(PGBENCH_TIME) -n -M $env(PGBENCH_QUERYMODE) -f fortunes.sql -j $env(PGBENCH_THREADS) -c $env(PGBENCH_CONNECTIONS) hello_world
 expect "Password:" {send "benchmarkdbpass\r"}
 interact
